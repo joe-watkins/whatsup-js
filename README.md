8a031326779eb3ccd76e140e9b72843d0f8b1db9
Joe Watkins <info@joe-watkins.io>
2014-01-31 09:46:39 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Introducing whatsUp.js
+A jQuery plugin written to make wrangling upcoming events easier.
+
+whatsUp.js leverages Date.js for date handling.
+
+## How Do I Use It?
+Include jQuery 1.7+ and whatsUp.js and bind then initialize the plugin by binding it to an element.
+
+```html
+ <script src="js/vendor/date.js"></script>
+ <script src="js/vendor/whatsup.js"></script>
+ <script>
+  $(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $("ul.dates").whatsUp();
+  });
+ </script>
+```
+
+The plugin will itterate through list items looking for a `data-date` attribute eg `data-date="01-Nov-2014"` by default. It will show you the next 5 events from today's date. There are many options you can set. You can even define a date window from today's date to show a certain number of events. The plugin can handle multiple date formats. Suggested format: `01-Nov-2014`
+
+## Changelog
+* _01.31.13 - v1.0.0 - 4kb_
+	* Initial release
+
+## Credits
+@_josephwatkins
---
+++ js/vendor/whatsup.js
@@ -0,0 +1,106 @@
+/*!
+* whatsUp
+* Author: Joe Watkins - joe-watkins.io
+* Licensed under the MIT license
+* http://james.padolsey.com/javascript/sorting-elements-with-jquery/
+* http://stackoverflow.com/questions/10636779/jquery-date-sorting
+*/
+(function($){
+ 
+    $.fn.whatsUp = function(options) {
+
+        var defaults = {
+          wrapper:       this,
+          items:          "li",
+          dataSelector:   "date",
+          output:         "default", // default,dateWindow
+          dateWindow:     7,
+          howMany:        5,
+          fakeDate:       "31-Jan-2014",
+          debug:          false,
+          rewrite:        false,
+          rewriteTarget:  "",
+          rewriteFormat:  "MMMM dS yyyy",
+          visibleClass:   "on"
+        }
+             
+        var options =  $.extend(defaults, options);
+        var o = options;
+
+          // run code
+          var $dates = o.wrapper.find(o.items);
+
+          if(o.fakeDate){
+            var today = Date.parse(o.fakeDate),
+                dateWindow = Date.parse(o.fakeDate).add(o.dateWindow).days();
+          }else{
+            var today = Date.today(),
+                dateWindow = Date.today().add(o.dateWindow).days();
+          }
+
+          $dates.each(function(index, item){
+            var itemDate = Date.parse($(this).data(o.dataSelector));
+
+            if(o.rewrite == true){
+              $(this).find(o.rewriteTarget).text(itemDate.toString(o.rewriteFormat));
+            }
+
+            if(o.output == 'default'){
+              var visibleEvents = $(o.wrapper).find('li.'+o.visibleClass).length;
+              
+              // add class to today's event if there is a match
+              var dateMatch = Date.compare(today, itemDate);
+              if(dateMatch == 0){
+                $(this).addClass(o.visibleClass);
+              }
+
+              // add active class to further events
+              if(visibleEvents < o.howMany){
+                if(!itemDate.isBefore(today)){ // the event isn't after window
+                    $(this).addClass(o.visibleClass);
+                }
+              }
+              
+            } // default
+
+            if(o.output == 'dateWindow'){
+              if(today.between(today, dateWindow) // today is in window
+                && !itemDate.isBefore(today) // the event isn't before today
+                && !itemDate.isAfter(dateWindow)){ // the event isn't after window
+                  $(this).addClass(o.visibleClass);
+              }
+            }// dateWindow
+
+            // what the heck is going on? check console.
+            if(o.debug === true){
+              testDates(today,itemDate,dateWindow);
+            }
+            
+          }); // each
+
+          // TODO DEAL WITH MIN / MAX
+
+          // check to make sure we o.howMany visible
+          var shownEvents = o.wrapper.find('.'+o.visibleClass).length,
+              eventDiff = o.howMany - shownEvents;
+
+              for (var i = 0; i < eventDiff; i++) {
+                o.wrapper.find('.on').last().next().addClass('on');
+              } 
+
+            // kill unused events from dom
+            o.wrapper.find(o.items+':not(.'+o.visibleClass+')').remove();
+        
+
+          function testDates(today,itemDate,dateWindow){
+            console.log('today: '+today);
+            console.log('item date: '+itemDate);
+            console.log('date window: '+dateWindow);
+            console.log('in window? '+itemDate.between(today, dateWindow)); // true|false
+            console.log('is after today? '+itemDate.isAfter(today)); // true|false
+            console.log('________________________________________');
+          } // testdates
+      
+    }; // $.fn
+
+}(jQuery));
